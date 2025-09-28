35ce5169dc2364be5dd2c19a3f741b6e8ae5db84
kevindav22 <kevindapit22@gmail.com>
2025-09-28 15:22:19 +0700

    update

--- src/components/pdf/pdfpage.jsx
+++ src/components/pdf/pdfpage.jsx
@@ -2,7 +2,7 @@
 const PdfPage = () => {
   return (
     <section className="w-full h-screen bg-gray-100 flex items-center justify-center">
-      <embed src="/src/assets/cv-kevin-david.pdf" type="application/pdf" className="w-full h-full" />
+      <embed src="/cv-kevin-david.pdf" type="application/pdf" className="w-full h-full" />
     </section>
   );
 };
